bba2723932c1f04307e4d2dfcd17c8396d228fa4
Diego Romero <dromero@MBP-de-Diego.fibertel.com.ar>
2021-05-11 21:00:33 -0300

    home design

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    const btnSwitch = document.querySelector('#switch');
 
+    btnSwitch.addEventListener('click', () => {
+      document.body.classList.toggle('dark');
+      btnSwitch.classList.toggle('active');
+    });
+  }
 }
