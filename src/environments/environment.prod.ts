c88b24afdf5f79c1ae9b5b191a79c99b63ac63dd
cepedaian <49700493+cepedaian@users.noreply.github.com>
2021-07-19 19:20:30 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   ambiente: 'v1.0',
-  url_server: 'http://localhost:18080',
+  url_server: 'http://lapicito-env.eba-u9q7agyc.us-east-2.elasticbeanstalk.com',
 };
