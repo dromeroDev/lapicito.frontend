fd3bcf665793a19a2c952c0e915912ccc7005562
Personal <personal@MacNXDiego.local>
2023-09-24 02:15:30 -0300

    fix ux image profile and space detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginGuard } from './core/guards/login.guard';
+import { FeedComponent } from './modules/feed/feed.component';
 import { HomeComponent } from './modules/home/home.component';
+import { LapicitoSuccessComponent } from './modules/lapicito/lapicito-success/lapicito-success.component';
+import { PublishmentCreateComponent } from './modules/publishment/publishment-create/publishment-create.component';
+import { PublishmentDetailComponent } from './modules/publishment/publishment-detail/publishment-detail.component';
+import { SpaceDetailComponent } from './modules/space/space-detail/space-detail.component';
+import { CallbackComponent } from './modules/user/callback/callback.component';
 import { PreferenceComponent } from './modules/user/preference/preference.component';
 import { ProfileComponent } from './modules/user/profile/profile.component';
 import { RegisterComponent } from './modules/user/register/register.component';
@@ -30,7 +36,7 @@ const routes: Routes = [
     component: ProfileComponent,
     canActivate: [LoginGuard],
   },
-  /* {
+  {
     path: 'feed',
     component: FeedComponent,
     canActivate: [LoginGuard],
@@ -58,7 +64,7 @@ const routes: Routes = [
   {
     path: 'lapicito/success',
     component: LapicitoSuccessComponent,
-  }, */
+  },
   {
     path: '**',
     component: HomeComponent,
--- src/app/modules/user/login/login.component.ts
+++ src/app/modules/user/login/login.component.ts
@@ -62,12 +62,12 @@ export class LoginComponent implements OnInit {
             this.storageService.setItem('usuario', JSON.stringify(data));
 
             this.close();
-            this.router.navigate(['/preference']);
-            /* if (res['tieneCategorias']) {
+
+            if (res['tieneCategorias']) {
               this.router.navigate(['/feed']);
             } else {
               this.router.navigate(['/preference']);
-            } */
+            }
           });
       },
       (error) => {
